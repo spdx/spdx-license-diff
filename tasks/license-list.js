d2ac02751ff3fef0b81eb8e72da300af0eef1a51
Alan D. Tse <alandtse@gmail.com>
2018-04-19 12:19:49 +0200

    Updating build script to generate spdx.txt
    Signed-off-by: Alan D. Tse <alandtse@gmail.com>

--- tasks/license-list.js
+++ tasks/license-list.js
@@ -3,10 +3,16 @@ import gulpif from 'gulp-if'
 import livereload from 'gulp-livereload'
 import args from './lib/args'
 var fs = require('fs');
-
-
+var exec = require('child_process').exec;
+ 
 gulp.task('license-list', () => {
-  return gulp.src(['app/license-list/spdx.txt','app/license-list/text/*.txt'])
+  process.chdir('license-list/text')
+  exec('ls *.txt > ../spdx.txt', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+  });
+  process.chdir('../..')
+  return gulp.src(['license-list/spdx.txt','license-list/text/*.txt'])
     .pipe(gulp.dest(`dist/${args.vendor}/license-list`))
     .pipe(gulpif(args.watch, livereload()))
 })
