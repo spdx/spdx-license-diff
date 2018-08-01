78045668062ef99f9382d269c51456dfe9128a69
Alan D. Tse <alandtse@gmail.com>
2018-07-31 22:50:08 -0700

    Breaking out stackoverflow code to own file
    Signed-off-by: Alan D. Tse <alandtse@gmail.com>

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -4,8 +4,7 @@ if(process.env.NODE_ENV === 'development'){
   require('chromereload/devonly')
 }
 
-//import './cc-by-sa.js'
-// content.js
+import { selectRangeCoords, getSelectionText} from './cc-by-sa.js'
 
 var selectedLicense = "";
 var spdx = null;
@@ -481,34 +480,4 @@ function restore_options() {
     }
     loadList();
   });
-}
-//https://stackoverflow.com/questions/2031518/javascript-selection-range-coordinates
-function selectRangeCoords(){
-  var node = window.getSelection();
-  var $span= $("<span/>");
-  var newRange = document.createRange();
-  newRange.setStart(node.focusNode, 0);
-  newRange.insertNode($span[0]); // using 'range' here instead of newRange unselects or causes flicker on chrome/webkit
-
-  var posX = $span.offset().left;
-  var posY = $span.offset().top;
-  $span.remove();
-  return [posX, posY]
-}
-
-  //https://stackoverflow.com/questions/5379120/get-the-highlighted-selected-text
-function getSelectionText() {
-  var text = "";
-  var activeEl = document.activeElement;
-  var activeElTagName = activeEl ? activeEl.tagName.toLowerCase() : null;
-  if (
-    (activeElTagName == "textarea") || (activeElTagName == "input" &&
-    /^(?:text|search|password|tel|url)$/i.test(activeEl.type)) &&
-    (typeof activeEl.selectionStart == "number")
-  ) {
-    text = activeEl.value.slice(activeEl.selectionStart, activeEl.selectionEnd);
-  } else if (window.getSelection) {
-    text = window.getSelection().toString();
-  }
-  return text;
 }
