7d73685b7102379c5bd61bfdfdbcdceffa0eb6cc
Alan D. Tse <alandtse@gmail.com>
2018-12-15 19:17:51 -0800

    Clean up code based on jshint
    Signed-off-by: Alan D. Tse <alandtse@gmail.com>

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,10 +1,10 @@
-"use strict";
+/*jshint esversion: 6 */
 // Enable chromereload by uncommenting this line:
-if(process.env.NODE_ENV === 'development' && typeof browser === "undefined"){
-  require('chromereload/devonly')
-}
+// if(process.env.NODE_ENV === 'development' && typeof browser === "undefined"){
+//   require('chromereload/devonly');
+// }
 
-import { selectRangeCoords, getSelectionText} from './cc-by-sa.js'
+import { selectRangeCoords, getSelectionText} from './cc-by-sa.js';
 var selectedLicense = "";
 var spdx = null;
 var selection = "";
@@ -35,7 +35,7 @@ chrome.runtime.onMessage.addListener(
         var posX = selectCoords[0], posY = selectCoords[1];
         renderBubble(posX, posY, selection);
         if (spdx && getSelectionText() == lastselection){ //diff previously done on selection
-          processLicenses((options.showBest == 0)? spdx.length: options.showBest, processTime)
+          processLicenses((options.showBest == 0)? spdx.length: options.showBest, processTime);
           return;
         } else {
           lastselection = selection;
@@ -47,55 +47,54 @@ chrome.runtime.onMessage.addListener(
           diffdisplayed = false;
         }
         ms_start = (new Date()).getTime();
-        compareSelection(selection)
+        compareSelection(selection);
       } else {
         updateBubbleText('No selection to compare; please select');
       }
     break;
     case "progressbarmax":
-      updateProgressBar(request.value, 0 ? request.reset : null)
+      updateProgressBar(request.value, 0 ? request.reset : null);
       updateBubbleText(request.stage);
     break;
     case "progressbarvalue":
-      updateProgressBar(-1, request.value)
+      updateProgressBar(-1, request.value);
     break;
     case "next":
-      updateProgressBar(-1, -1)
+      updateProgressBar(-1, -1);
     break;
     case "sortdone":
       spdx = request.result;
       var ms_end = (new Date()).getTime();
       processTime = ms_end - ms_start;
       console.log("processTime: " + processTime/1000 + ("s"));
-      processLicenses((options.showBest == 0)? spdx.length: options.showBest, processTime)
+      processLicenses((options.showBest == 0)? spdx.length: options.showBest, processTime);
     break;
     case "diffnext":
-      updateProgressBar(-1, -1)
-      diffsdone++
+      updateProgressBar(-1, -1);
+      diffsdone++;
       var threadid = request.id;
       var result = request.result;
       var spdxid = request.spdxid;
       var record = request.record;
       var details = request.details;
-      spdx[record]["html"] = result["html"]
-      spdx[record]["time"] = result["time"]
+      spdx[record].html = result.html;
+      spdx[record].time = result.time;
       for (var k in details){
         spdx[record][k] = details[k];
       }
-      var select = $("#licenses option[value='" + spdxid + "']").removeAttr("disabled")
-      console.log("%s: Received diff for %s %s/%s", threadid, spdxid, diffsdone, diffsdue)
+      var select = $("#licenses option[value='" + spdxid + "']").removeAttr("disabled");
+      console.log("%s: Received diff for %s %s/%s", threadid, spdxid, diffsdone, diffsdue);
       if (diffdisplayed)
         return;
       if (diffsdone >= diffsdue){
-        console.log("All diffs complete")
-        displayDiff(spdx[0]["html"], spdx[0]["time"])
-      } else if (spdx[0]["html"]){
-        console.log("Best diff received; we can display")
-        displayDiff(spdx[0]["html"], spdx[0]["time"])
+        console.log("All diffs complete");
+        displayDiff(spdx[0].html, spdx[0].time);
+      } else if (spdx[0].html){
+        console.log("Best diff received; we can display");
+        displayDiff(spdx[0].html, spdx[0].time);
       }
     break;
     default:
-    break;
     return true;
   }
 });
@@ -103,7 +102,7 @@ chrome.runtime.onMessage.addListener(
 //This function responds to changes to storage
 chrome.storage.onChanged.addListener(function(changes, area) {
     if (area == "local" && "options" in changes) {
-      console.log("Detected changed options; reloading")
+      console.log("Detected changed options; reloading");
       restore_options();
     }
 });
@@ -118,22 +117,22 @@ function compareSelection(selection){
 
 // This will begin displaying diffs based off sorted list spdx
 function processLicenses(showBest, processTime=0){
-  console.log("Processing diffs for %s items exceeding %s% match", showBest, Number(options.minpercentage))
-  if (spdx && (spdx.length == 0 || Number(spdx[0]["percentage"]) <= Number(options.minpercentage))){
+  console.log("Processing diffs for %s items exceeding %s% match", showBest, Number(options.minpercentage));
+  if (spdx && (spdx.length == 0 || Number(spdx[0].percentage) <= Number(options.minpercentage))){
     console.log("No results to display");
     displayDiff(null, processTime);
-    updateProgressBar(spdx.length, spdx.length)
-    return
+    updateProgressBar(spdx.length, spdx.length);
+    return;
   } else if (spdx && diffdisplayed) {
-    addSelectFormFromArray("licenses", spdx, showBest, options.minpercentage)
-    displayDiff(spdx[0]["html"], spdx[0]["time"]);
+    addSelectFormFromArray("licenses", spdx, showBest, options.minpercentage);
+    displayDiff(spdx[0].html, spdx[0].time);
   } else {
-    updateBubbleText("Diffing results")
+    updateBubbleText("Diffing results");
     for (var i = 0; i < showBest; i++){
-      var license = spdx[i]["spdxid"];
-      var data = spdx[i]["difftext"];
-      var distance = spdx[i]["distance"];
-      var percentage = spdx[i]["percentage"];
+      var license = spdx[i].spdxid;
+      var data = spdx[i].difftext;
+      var distance = spdx[i].distance;
+      var percentage = spdx[i].percentage;
       if (i == 0) {
         selectedLicense = license;
         console.log("Best match of " + showBest + " : " + license + ": " + distance + " (" + percentage+ "%)");
@@ -145,8 +144,8 @@ function processLicenses(showBest, processTime=0){
       chrome.runtime.sendMessage({'command':"generateDiff", 'selection': selection, 'spdxid':license,'license':data, 'record':i});
       diffsdue++;
     }
-    updateProgressBar(diffsdue, 0)
-    addSelectFormFromArray("licenses", spdx, showBest, options.minpercentage)
+    updateProgressBar(diffsdue, 0);
+    addSelectFormFromArray("licenses", spdx, showBest, options.minpercentage);
   }
 }
 
@@ -155,22 +154,22 @@ function displayDiff(html, time=processTime){
   diffdisplayed = true;
   if (!html){
     updateBubbleText('Time: '+time/ 1000+' s<br />No results to display');
-    return
+    return;
   }
-  var html = spdx[0]["html"];
-  var time = spdx[0]["time"];
-  var spdxid = spdx[0]["spdxid"];
-  var title = `<a href="https://spdx.org/licenses/${spdxid}.html" target="_blank">${spdxid}</a>`
-  var timehtml = ' processed in '+(time+processTime)/ 1000+'s<br />'
+  html = spdx[0].html;
+  time = spdx[0].time;
+  var spdxid = spdx[0].spdxid;
+  var title = `<a href="https://spdx.org/licenses/${spdxid}.html">${spdxid}</a>`;
+  var timehtml = ' processed in '+(time+processTime)/ 1000+'s<br />';
   updateBubbleText(title + timehtml + html);
   var el = document.getElementById("licenses").addEventListener("change", function () {
     if (this.value != selectedLicense){
       selectedLicense = this.value;
-      spdxid = spdx[this.options.selectedIndex]["spdxid"]
-      html = spdx[this.options.selectedIndex]["html"];
-      time = spdx[this.options.selectedIndex]["time"];
-      title = `<a href="https://spdx.org/licenses/${spdxid}.html" target="_blank">${spdxid}</a>`
-      timehtml = ' processed in '+(time+processTime)/ 1000+'s<br />'
+      spdxid = spdx[this.options.selectedIndex].spdxid;
+      html = spdx[this.options.selectedIndex].html;
+      time = spdx[this.options.selectedIndex].time;
+      title = `<a href="https://spdx.org/licenses/${spdxid}.html" target="_blank">${spdxid}</a>`;
+      timehtml = ' processed in '+(time+processTime)/ 1000+'s<br />';
       updateBubbleText(title + timehtml + html);
     } else {
 
@@ -180,21 +179,22 @@ function displayDiff(html, time=processTime){
 
 //This function will create a select form with the sorted licenses in arr
 function addSelectFormFromArray(id, arr, number=arr.length, minimum=0) {
-  if (form = document.getElementById(id))
+  var form = document.getElementById(id);
+  if (form)
     form.outerHTML="";
   if (!$('#license_form').length){
     var bubbleDOM = $('#license_bubble')[0];
     var bubbleDOMText = $('#bubble_text')[0];
-    var form = bubbleDOM.insertBefore(document.createElement('form'), bubbleDOMText);
+    form = bubbleDOM.insertBefore(document.createElement('form'), bubbleDOMText);
     form.setAttribute('id',"license_form");
   }
   form = document.getElementById("license_form");
   var select = form.appendChild(document.createElement('select'));
   select.id = id;
   for (var i=0; i < arr.length && i < number; i++){
-    var value = arr[i]["spdxid"];
-    var percentage = arr[i]["percentage"]
-    var text = value + " : " + arr[i]["distance"] + " differences ("+ percentage +"% match)";
+    var value = arr[i].spdxid;
+    var percentage = arr[i].percentage;
+    var text = value + " : " + arr[i].distance + " differences ("+ percentage +"% match)";
     if (Number(percentage) < Number(minimum)){ //No match at all
       break;
     }
@@ -202,7 +202,7 @@ function addSelectFormFromArray(id, arr, number=arr.length, minimum=0) {
     option.value = value;
     option.text = text;
     if (diffsdone ==0)
-      option.setAttribute("disabled","disabled")
+      option.setAttribute("disabled","disabled");
   }
 }
 
@@ -241,7 +241,7 @@ document.addEventListener('mousedown', function (e) {
 
 // Move that bubble to the appropriate location.
 function renderBubble(mouseX, mouseY, selection) {
-  updateProgressBar(-1, 1, true)
+  updateProgressBar(-1, 1, true);
   var progressbar = $('#progress_bubble')[0];
   updateBubbleText("Processing...");
   var bubbleDOM = $('#license_bubble')[0];
@@ -264,7 +264,7 @@ function updateBubbleText(text) {
 // else incremented by absolute value for negative numbers
 function updateProgressBar(max, value, visible=true) {
   var progressbar = $('#progress_bubble')[0];
-  progressbar.style.visibility = visible ?'visible': 'hidden'
+  progressbar.style.visibility = visible ?'visible': 'hidden';
   if (max > 0){
     progressbar.setAttribute('max',max);
   }
@@ -272,7 +272,7 @@ function updateProgressBar(max, value, visible=true) {
     if (value >= 0) {
       progressbar.value = value;
     } else if (progressbar.value < progressbar.getAttribute('max')){
-      progressbar.value = progressbar.value + Math.abs(value)
+      progressbar.value = progressbar.value + Math.abs(value);
     }
   }
 }
@@ -292,5 +292,5 @@ function restore_options() {
   });
 }
 
-console.log("Spdx-diff ContentScript loaded")
+console.log("Spdx-diff ContentScript loaded");
 true;
