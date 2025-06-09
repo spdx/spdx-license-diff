7b21be537908af874c0e32bb47e3b9325f0b5c11
Alan D. Tse <alandtse@gmail.com>
2025-06-09 00:45:55 -0700

    build: add ESLint config and update dependencies
    - Add modern ESLint configuration with browser extension globals
    - Update package.json with new dev dependencies and scripts
    - Configure webextension-toolbox for improved build process
    - Update yarn.lock for dependency changes
    
    Signed-off-by: Alan D. Tse <alandtse@gmail.com>

--- webextension-toolbox-config.js
+++ webextension-toolbox-config.js
@@ -4,37 +4,21 @@
 const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-  webpack: (config, { dev, vendor }) => {
+  webpack: (config, { dev: _dev, vendor: _vendor }) => {
     config.plugins.unshift( //  unshift to run plugin first
-      new CopyPlugin([
-        {
-          from: '../LICENSE',
-          to: config.target
-        },
-        {
-          from: '../oss-attribution/attribution.txt',
-          to: config.target
-        }
-      ])
-
-      // unnecessary to add actual dependencies with proper import of modules
-    //   new webpack.ProvidePlugin({
-    //     $: 'jquery',
-    //     jQuery: 'jquery'
-    //   new webpack.ProvidePlugin({
-    //     _: 'underscore',
-    //     underscore: 'underscore'
-    //   }),
-    //   new webpack.ProvidePlugin({
-    //     Levenshtein: 'fast-levenshtein'
-    //   }),
-    //   new webpack.ProvidePlugin({
-    //     DiffMatchPatch: 'diff-match-patch'
-    //   }),
-    //   new webpack.ProvidePlugin({
-    //     md5: 'md5-jkmyers'
-    //   })
-    )
+      new CopyPlugin({
+        patterns: [
+          {
+            from: '../LICENSE',
+            to: config.target
+          },
+          {
+            from: '../oss-attribution/attribution.txt',
+            to: config.target
+          }
+        ]
+      })
+    );
 
     return config
   }
