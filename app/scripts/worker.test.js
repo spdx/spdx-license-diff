2b7058c1f51d1c2ba86ee4f8c99aea6a1ddb512a
Alan D. Tse <alandtse@gmail.com>
2026-06-19 10:55:16 -0700

    chore: add SPDX headers to config and test files
    Signed-off-by: Alan D. Tse <alandtse@gmail.com>

--- app/scripts/worker.test.js
+++ app/scripts/worker.test.js
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: Alan D. Tse <alandtse@gmail.com>
+// SPDX-License-Identifier: (GPL-3.0-or-later AND Apache-2.0)
+
 /* eslint-env jest, node */
 
 global.self = global;
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: Alan D. Tse <alandtse@gmail.com>
+// SPDX-License-Identifier: CC0-1.0
+
 module.exports = {
   presets: [
     ['@babel/preset-env', { targets: { node: 'current' } }]
